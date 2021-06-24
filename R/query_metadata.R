4be3f046e00e4edb0cb0e0b17ae6ae9f67530ef9
vjcitn <stvjc@channing.harvard.edu>
2021-06-24 15:11:17 -0400

    range of queries

--- R/query_metadata.R
+++ R/query_metadata.R
@@ -3,24 +3,42 @@
 #' @import jsonlite
 #' @import httr
 #' @param luceneq character(1) query in lucene syntax
+#' @param component character(1) API component to use, defaults to `StudiesSraStudiesGet`
 #' @param \dots passed to omicidxClientR::ExperimentsSraExperimentsGet
+#' @note Queries for "nested" fields like organism within sample are handled in the third example.
 #' @examples
+#' # example 1
 #' x = query_metadata("cancer")
-#' str(httr::response(x))
+#' x
+#' sapply(x$hits, "[[", "title")
+#' # example 2 fix on RNA-seq
+#' y = query_metadata('cancer AND library_strategy:"RNA-Seq"', 
+#'    component="ExperimentsSraExperimentsGet")
+#' table(sapply(y$hits, "[[", "library_strategy"))
+#' # example 3 fix on RNA-seq in humans
+#' yy =query_metadata('cancer AND library_strategy:"RNA-Seq" AND sample.organism:"Homo sapiens"',
+#'   component="ExperimentsSraExperimentsGet", size=100)
+#' table(sapply(yy$hits, function(x) x$sample[,"organism"]))
 #' @export
-query_metadata = function(luceneq, size=10, ...) {
+query_metadata = function(luceneq, component = "StudiesSraStudiesGet", size=10, ...) {
  client = ApiClient$new(basePath="https://api.omicidx.cancerdatasci.org")
  inst = SRAApi$new(apiClient=client)
- ans = inst$ExperimentsSraExperimentsGet( q = luceneq, size=size, ... )
+ ans = inst[[component]]( q = luceneq, size=size, ... )
  fin = list(hits=ans$hits, obj=ans, query=luceneq, time=Sys.time())
  class(fin) = "omicidx_qhits"
  fin
 }
 
 #' display hits from omicidx query
+#' @export
 print.omicidx_qhits = function(x, ...) {
  cat(sprintf("omicidx response with %d hits.\n", length(x$hits)))
 }
  
   
-
+#' show endpoints for SRAApi
+#' @export
+SRAApiView = function() {
+  client = ApiClient$new(basePath="https://api.omicidx.cancerdatasci.org")
+  SRAApi$new(apiClient=client)
+}
