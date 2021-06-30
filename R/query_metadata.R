fad2a2e8c3103e82c6f5ed43bd1a30fcfdd0ebb2
vjcitn <stvjc@channing.harvard.edu>
2021-06-30 18:22:04 -0400

    update

--- R/query_metadata.R
+++ R/query_metadata.R
@@ -4,8 +4,11 @@
 #' @import httr
 #' @param luceneq character(1) query in lucene syntax
 #' @param component character(1) API component to use, defaults to `StudiesSraStudiesGet`
+#' @param size numeric(1) number of records to return, defaults to 10.
 #' @param \dots passed to omicidxClientR::ExperimentsSraExperimentsGet
 #' @note Queries for "nested" fields like organism within sample are handled in the third example.
+#' Cursor information will be present in `obj` element of the return value.
+#' @return list with S3 class `omicidx_qhits`, elements are hits, obj, query, time.
 #' @examples
 #' # example 1
 #' x = query_metadata("cancer")
@@ -30,6 +33,8 @@ query_metadata = function(luceneq, component = "StudiesSraStudiesGet", size=10, 
 }
 
 #' display hits from omicidx query
+#' @param x instance of `omicidx_qhits`
+#' @param \dots not used
 #' @export
 print.omicidx_qhits = function(x, ...) {
  cat(sprintf("omicidx response with %d hits.\n", length(x$hits)))
